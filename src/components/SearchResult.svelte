af8285bd071fe708cfbff9e94b05d4e2eae4062f
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2022-08-07 19:09:47 +0200

    search result component styling

--- src/components/SearchResult.svelte
+++ src/components/SearchResult.svelte
@@ -3,7 +3,41 @@
 
 </script>
 
-<div class="search-result">
-  <p>{searchResult.title}</p>
-  <a href={searchResult.url}>Link</a>
+<div class="result">
+  <a href={searchResult.url} target="_blank">
+    <img src="/images/wikipedia.png" alt="wikipedia-logo" />
+  </a>
+  <p class="text">{searchResult.title}</p>
 </div>
+
+<style>
+  .result {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border: 2px solid black;
+    border-radius: 0.25rem;
+    user-select: none;
+    padding: 0 0.5rem;
+  }
+
+  a {
+    text-decoration: none;
+    height: 1.5rem;
+    width: 1.5rem;
+  }
+
+  img {
+    width: 1.5rem;
+    height: 1.5rem;
+    background-size: contain;
+    background-position: center center;
+    background-repeat: no-repeat;
+  }
+
+  .result .text {
+    font-size: 1.5rem;
+    flex: 1;
+    padding-left: 0.5rem;
+  }
+</style>
