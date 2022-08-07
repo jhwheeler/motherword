abf308511136c471af72499d732f23a280548626
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2022-08-07 19:44:04 +0200

    constants file

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+  export const DEFAULT_SOURCE_LANGUAGE = {
+    code: 'en',
+    name: 'English',
+  }
+
+  export const DEFAULT_TARGET_LANGUAGE = {
+    code: 'sv',
+    name: 'Swedish',
+  }
+
+  export const DISAMBIGUATION_CATEGORY = 'Category:All disambiguation pages'
+
